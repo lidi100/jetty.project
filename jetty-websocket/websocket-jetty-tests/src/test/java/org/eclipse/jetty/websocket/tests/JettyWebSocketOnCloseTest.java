07600b8c9ac72e4388abfe78819817ab503733d7
Lachlan Roberts <lachlan@webtide.com>
2020-01-24 14:05:44 +1100

    Issue 4502 - add tests for javax onClose event
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-websocket/websocket-jetty-tests/src/test/java/org/eclipse/jetty/websocket/tests/JettyWebSocketOnCloseTest.java
+++ jetty-websocket/websocket-jetty-tests/src/test/java/org/eclipse/jetty/websocket/tests/JettyWebSocketOnCloseTest.java
@@ -23,7 +23,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class WebSocketOnCloseTest
+public class JettyWebSocketOnCloseTest
 {
     private Server server;
     private ServerConnector connector;
